6ebd7af2789a50a26f8a88b88c28ac8621e3f897
Adamouization <adam@jamour.com>
2019-08-06 09:27:31 +0200

    Initial Commit

---
+++ MealTrackerTests/MealTrackerTests.swift
@@ -0,0 +1,34 @@
+//
+//  MealTrackerTests.swift
+//  MealTrackerTests
+//
+//  Created by Adam Jaamour on 06/08/2019.
+//  Copyright © 2019 Adam Jaamour. All rights reserved.
+//
+
+import XCTest
+@testable import MealTracker
+
+class MealTrackerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
