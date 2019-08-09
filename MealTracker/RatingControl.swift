d45747235c953452d123e3ebcef7c26e08e3d45c
Adamouization <adam@jamour.com>
2019-08-09 11:56:19 +0200

    Start implementing rating system button

---
+++ MealTracker/RatingControl.swift
@@ -0,0 +1,49 @@
+//
+//  RatingControl.swift
+//  MealTracker
+//
+//  Created by Adam Jaamour on 08/08/2019.
+//  Copyright © 2019 Adam Jaamour. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    //MARK: Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+    
+    //MARK: Button Action
+    
+    @objc func ratingButtonTapped(button: UIButton) {
+        print("Button pressed 👍")
+    }
+    
+    //MARK: Private Methods
+    
+    private func setupButtons() {
+        // Create the button
+        let button = UIButton()
+        button.backgroundColor = UIColor.red
+        
+        // Add constraints
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        // Setup the button action
+        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+        
+        // Add the button to the stack
+        addArrangedSubview(button)
+    }
+}
