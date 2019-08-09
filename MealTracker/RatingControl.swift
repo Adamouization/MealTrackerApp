24b429552bdb2c1cb51815eeea2252d6bf4cebd0
Adam Jaamour <adam@jamour.com>
2019-08-09 12:39:03 +0200

    Functional 5-star rating system

--- MealTracker/RatingControl.swift
+++ MealTracker/RatingControl.swift
@@ -13,7 +13,11 @@ import UIKit
     //MARK: Properties
     
     private var ratingButtons = [UIButton]()  // Only accessible inside RatingControl class.
-    var rating = 0
+    var rating = 0 {
+        didSet {
+            updateButtonSelectionStates()
+        }
+    }
     @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
         didSet {  // Called immediately after the property’s value is set.
             setupButtons()
@@ -40,7 +44,20 @@ import UIKit
     //MARK: Button Action
     
     @objc func ratingButtonTapped(button: UIButton) {
-        print("Button pressed 👍")
+        guard let index = ratingButtons.index(of: button) else {
+            fatalError("The button, \(button), is not in the ratingButtons array: \(ratingButtons)")
+        }
+        
+        // Calculate the rating of the selected button
+        let selectedRating = index + 1
+        
+        if selectedRating == rating {
+            // If the selected star represents the current rating, reset the rating to 0.
+            rating = 0
+        } else {
+            // Otherwise set the rating to the selected star
+            rating = selectedRating
+        }
     }
     
     //MARK: Private Methods
@@ -54,11 +71,22 @@ import UIKit
         }
         ratingButtons.removeAll()  // Remove all buttons from the rating button array.
         
+        // Load Button Images
+        let bundle = Bundle(for: type(of: self))
+        let filledStar = UIImage(named: "filledStar", in: bundle, compatibleWith: self.traitCollection)
+        let emptyStar = UIImage(named:"emptyStar", in: bundle, compatibleWith: self.traitCollection)
+        let highlightedStar = UIImage(named:"highlightedStar", in: bundle, compatibleWith: self.traitCollection)
+        
         // Create 5 buttons.
         for _ in 0..<starCount {
             // Create a single button.
             let button = UIButton()
-            button.backgroundColor = UIColor.red
+            
+            // Set the button images
+            button.setImage(emptyStar, for: .normal)
+            button.setImage(filledStar, for: .selected)
+            button.setImage(highlightedStar, for: .highlighted)
+            button.setImage(highlightedStar, for: [.highlighted, .selected])
             
             // Add constraints.
             button.translatesAutoresizingMaskIntoConstraints = false
@@ -73,6 +101,16 @@ import UIKit
             
             // Add the new button to the rating button array.
             ratingButtons.append(button)
+        }
+        
+        updateButtonSelectionStates()
+    }
+    
+    /// Updates the selection state of the buttons.
+    private func updateButtonSelectionStates() {
+        for (index, button) in ratingButtons.enumerated() {
+            // If the index of a button is less than the rating, that button should be selected.
+            button.isSelected = index < rating
         }
     }
     
