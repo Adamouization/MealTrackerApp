120e15bd8685392066836a4d49b9b93b1ca00b83
Adam Jaamour <adam@jamour.com>
2019-08-29 12:19:09 +0200

    Create viewcontroller for tableview

---
+++ MealTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  MealTracker
+//
+//  Created by Adam Jaamour on 29/08/2019.
+//  Copyright © 2019 Adam Jaamour. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
