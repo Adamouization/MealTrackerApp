76f5745cea43bf7899592610872b6d9f36c93647
Adam Jaamour <adam@jamour.com>
2019-08-29 13:53:28 +0200

    Remove meal label from meal view + refactoring

--- MealTracker/MealViewController.swift
+++ MealTracker/MealViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MealViewController.swift
 //  MealTracker
 //
 //  Created by Adam Jaamour on 06/08/2019.
@@ -8,12 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     //MARK: Properties
     
     @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
     
@@ -34,7 +33,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
-        mealNameLabel.text = textField.text  // only when pressing enter
+        // add feature
     }
     
     //MARK: UIImagePickerControllerDelegate
