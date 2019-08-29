a893e3769a7546da5669dd8621459decfba85308
Adam Jaamour <adam@jamour.com>
2019-08-29 10:27:53 +0200

    Add more robust tests + modify Meal class guard

--- MealTrackerAppTests/MealTrackerAppTests.swift
+++ MealTrackerAppTests/MealTrackerAppTests.swift
@@ -13,7 +13,7 @@ class MealTrackerAppTests: XCTestCase {
 
     //MARK: Meal Class Tests
     
-    // Confirm that the Meal initializer returns a Meal object when passed valid parameters.
+    /// Confirm that the Meal initializer returns a Meal object when passed valid parameters.
     func testMealInitializationSucceeds() {
         
         // Zero rating
@@ -26,12 +26,16 @@ class MealTrackerAppTests: XCTestCase {
         
     }
     
-    // Confirm that the Meal initialier returns nil when passed a negative rating or an empty name.
+    /// Confirm that the Meal initialier returns nil when passed a negative rating or an empty name.
     func testMealInitializationFails() {
         
         // Negative rating
         let negativeRatingMeal = Meal.init(name: "Negative", photo: nil, rating: -1)
         XCTAssertNil(negativeRatingMeal)
+        
+        // Rating exceeds maximum
+        let largeRatingMeal = Meal.init(name: "Large", photo: nil, rating: 6)
+        XCTAssertNil(largeRatingMeal)
         
         // Empty String
         let emptyStringMeal = Meal.init(name: "", photo: nil, rating: 0)
